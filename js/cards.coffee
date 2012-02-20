88b5430f583eb5fb2219f5319ddd8dc4b635002a
Pawel Decowski <pawel.decowski@gmail.com>
2012-02-20 10:55:38 +0000

    Refactoring
    * check card type by looping through an array of cards' data
      (name, pattern) instead of having a separate method for each
      card type
    * demo: change all CSS to LESS
    * demo: import reset.less inside the main LESS file

--- js/cards.coffee
+++ js/cards.coffee
@@ -1,44 +1,60 @@
 $ = jQuery
 
 $.fn.cards = (callback) ->
-    is_visa = (number) ->
-        number.match /^4/
-
-    is_visa_electron = (number) ->
-        number.match /^(4026|417500|4508|4844|491(3|7))/
-
-    is_mastercard = (number) ->
-        number.match /^5[1-5]/
-
-    is_maestro = (number) ->
-        number.match /^(5018|5020|5038|6304|6759|676[1-3])/
-
-    is_discover = (number) ->
-        number.match /^(6011|622(12[6-9]|1[3-9][0-9]|[2-8][0-9]{2}|9[0-1][0-9]|92[0-5]|64[4-9])|65)/
+    cards = [
+        {
+            name: 'visa-electron'
+            pattern: /^(4026|417500|4508|4844|491(3|7))/
+        }
+        {
+            name: 'visa'
+            pattern: /^4/
+        }
+        {
+            name: 'mastercard'
+            pattern: /^5[1-5]/
+        }
+        {
+            name: 'maestro'
+            pattern: /^(5018|5020|5038|6304|6759|676[1-3])/
+        }
+        {
+            name: 'discover'
+            pattern: /^(6011|622(12[6-9]|1[3-9][0-9]|[2-8][0-9]{2}|9[0-1][0-9]|92[0-5]|64[4-9])|65)/
+        }
+    ]
 
     check_card = (number) ->
-        if is_visa_electron number
-            callback 'visa-electron'
-            return
-        else if is_visa number
-            callback 'visa' 
-            return
-        else if is_maestro number
-            callback 'maestro'
-            return
-        else if is_discover number
-            callback 'discover'
-            return
-        else if is_mastercard number
-            callback 'mastercard'
-            return
+        for card in cards
+            if number.match card.pattern
+                return callback card.name
 
         callback ''
-        
+
+    validate_luhn = (number) ->
+        check_digit = number[number.length - 1]
+
+        sum = 0
+
+        for digit, n in number.split('').reverse().slice(1).join('')
+            n = parseInt n, 10
+            digit = parseInt digit, 10
+            if n % 2
+                sum += digit
+            else
+                digit *= 2
+                if digit < 10
+                    sum += digit
+                else
+                    sum += parseInt(digit.toString()[0], 10) + parseInt(digit.toString()[1], 10)
+
     this.bind('input', ->
-        number = $(this).val().replace(/[ -]/, '')
-        check_card(number)
+        number = $(this).val().replace /[ -]/, ''
+        validate_luhn number
+        check_card number
     )
 
-    number = $(this).val().replace(/[ -]/, '')
-    check_card(number)
+    number = $(this).val().replace /[ -]/, ''
+    check_card number
+
+    this
--- js/cards.js
+++ js/cards.js
@@ -4,48 +4,65 @@
   $ = jQuery;
 
   $.fn.cards = function(callback) {
-    var check_card, is_discover, is_maestro, is_mastercard, is_visa, is_visa_electron, number;
-    is_visa = function(number) {
-      return number.match(/^4/);
-    };
-    is_visa_electron = function(number) {
-      return number.match(/^(4026|417500|4508|4844|491(3|7))/);
-    };
-    is_mastercard = function(number) {
-      return number.match(/^5[1-5]/);
-    };
-    is_maestro = function(number) {
-      return number.match(/^(5018|5020|5038|6304|6759|676[1-3])/);
-    };
-    is_discover = function(number) {
-      return number.match(/^(6011|622(12[6-9]|1[3-9][0-9]|[2-8][0-9]{2}|9[0-1][0-9]|92[0-5]|64[4-9])|65)/);
-    };
+    var cards, check_card, number, validate_luhn;
+    cards = [
+      {
+        name: 'visa-electron',
+        pattern: /^(4026|417500|4508|4844|491(3|7))/
+      }, {
+        name: 'visa',
+        pattern: /^4/
+      }, {
+        name: 'mastercard',
+        pattern: /^5[1-5]/
+      }, {
+        name: 'maestro',
+        pattern: /^(5018|5020|5038|6304|6759|676[1-3])/
+      }, {
+        name: 'discover',
+        pattern: /^(6011|622(12[6-9]|1[3-9][0-9]|[2-8][0-9]{2}|9[0-1][0-9]|92[0-5]|64[4-9])|65)/
+      }
+    ];
     check_card = function(number) {
-      if (is_visa_electron(number)) {
-        callback('visa-electron');
-        return;
-      } else if (is_visa(number)) {
-        callback('visa');
-        return;
-      } else if (is_maestro(number)) {
-        callback('maestro');
-        return;
-      } else if (is_discover(number)) {
-        callback('discover');
-        return;
-      } else if (is_mastercard(number)) {
-        callback('mastercard');
-        return;
+      var card, _i, _len;
+      for (_i = 0, _len = cards.length; _i < _len; _i++) {
+        card = cards[_i];
+        if (number.match(card.pattern)) return callback(card.name);
       }
       return callback('');
+    };
+    validate_luhn = function(number) {
+      var check_digit, digit, n, sum, _len, _ref, _results;
+      check_digit = number[number.length - 1];
+      sum = 0;
+      _ref = number.split('').reverse().slice(1).join('');
+      _results = [];
+      for (n = 0, _len = _ref.length; n < _len; n++) {
+        digit = _ref[n];
+        n = parseInt(n, 10);
+        digit = parseInt(digit, 10);
+        if (n % 2) {
+          _results.push(sum += digit);
+        } else {
+          digit *= 2;
+          if (digit < 10) {
+            _results.push(sum += digit);
+          } else {
+            _results.push(sum += parseInt(digit.toString()[0], 10) + parseInt(digit.toString()[1], 10));
+          }
+        }
+      }
+      return _results;
     };
     this.bind('input', function() {
       var number;
       number = $(this).val().replace(/[ -]/, '');
+      validate_luhn(number);
       return check_card(number);
     });
     number = $(this).val().replace(/[ -]/, '');
-    return check_card(number);
+    check_card(number);
+    return this;
   };
 
 }).call(this);
