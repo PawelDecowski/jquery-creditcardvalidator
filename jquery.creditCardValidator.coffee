c910c8d34eb732f49525de6ec43ea99b6e8ac0dd
Pawel Decowski <pawel.decowski@gmail.com>
2012-02-20 12:27:33 +0000

    Added number length and Luhn validation

--- jquery.creditCardValidator.coffee
+++ jquery.creditCardValidator.coffee
@@ -1,60 +1,75 @@
 $ = jQuery
 
 $.fn.cards = (callback) ->
-    cards = [
+    card_types = [
         {
             name: 'visa-electron'
             pattern: /^(4026|417500|4508|4844|491(3|7))/
+            valid_length: [ 16 ]
         }
         {
             name: 'visa'
             pattern: /^4/
+            valid_length: [ 13, 16 ]
         }
         {
             name: 'mastercard'
             pattern: /^5[1-5]/
+            valid_length: [ 16 ]
         }
         {
             name: 'maestro'
             pattern: /^(5018|5020|5038|6304|6759|676[1-3])/
+            valid_length: [ 12..19 ]
         }
         {
             name: 'discover'
             pattern: /^(6011|622(12[6-9]|1[3-9][0-9]|[2-8][0-9]{2}|9[0-1][0-9]|92[0-5]|64[4-9])|65)/
+            valid_length: [ 16 ]
         }
     ]
 
-    check_card = (number) ->
-        for card in cards
-            if number.match card.pattern
-                return callback card.name
+    get_card_type = (number) ->
+        for card_type in card_types
+            if number.match card_type.pattern
+                return card_type
 
-        callback ''
+        null
 
-    validate_luhn = (number) ->
-        check_digit = number[number.length - 1]
-
+    is_valid_luhn = (number) ->
         sum = 0
 
-        for digit, n in number.split('').reverse().slice(1).join('')
-            n = parseInt n, 10
-            digit = parseInt digit, 10
+        for digit, n in number.split('').reverse().join('')
+            digit = +digit # the + casts the string to int 
             if n % 2
-                sum += digit
-            else
                 digit *= 2
-                if digit < 10
-                    sum += digit
-                else
-                    sum += parseInt(digit.toString()[0], 10) + parseInt(digit.toString()[1], 10)
+                if digit < 10 then sum += digit else sum += digit - 9
+            else
+                sum += digit
+        
+        sum % 10 == 0
+
+    is_valid_length = (number, card_type) ->
+        number.length in card_type.valid_length
+    
+    validate = (number) ->
+        card_type = get_card_type number
+
+        if card_type?
+            luhn = is_valid_luhn number
+            length = is_valid_length number, card_type
+
+        callback
+            card_type: card_type
+            luhn: luhn
+            length: length
 
     this.bind('input', ->
         number = $(this).val().replace /[ -]/, ''
-        validate_luhn number
-        check_card number
+        validate number
     )
 
     number = $(this).val().replace /[ -]/, ''
-    check_card number
+    validate number
 
     this
--- jquery.creditCardValidator.js
+++ jquery.creditCardValidator.js
@@ -1,67 +1,86 @@
 (function() {
-  var $;
+  var $,
+    __indexOf = Array.prototype.indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
   $ = jQuery;
 
   $.fn.cards = function(callback) {
-    var cards, check_card, number, validate_luhn;
-    cards = [
+    var card_types, get_card_type, is_valid_length, is_valid_luhn, number, validate;
+    card_types = [
       {
         name: 'visa-electron',
-        pattern: /^(4026|417500|4508|4844|491(3|7))/
+        pattern: /^(4026|417500|4508|4844|491(3|7))/,
+        valid_length: [16]
       }, {
         name: 'visa',
-        pattern: /^4/
+        pattern: /^4/,
+        valid_length: [13, 16]
       }, {
         name: 'mastercard',
-        pattern: /^5[1-5]/
+        pattern: /^5[1-5]/,
+        valid_length: [16]
       }, {
         name: 'maestro',
-        pattern: /^(5018|5020|5038|6304|6759|676[1-3])/
+        pattern: /^(5018|5020|5038|6304|6759|676[1-3])/,
+        valid_length: [12, 13, 14, 15, 16, 17, 18, 19]
       }, {
         name: 'discover',
-        pattern: /^(6011|622(12[6-9]|1[3-9][0-9]|[2-8][0-9]{2}|9[0-1][0-9]|92[0-5]|64[4-9])|65)/
+        pattern: /^(6011|622(12[6-9]|1[3-9][0-9]|[2-8][0-9]{2}|9[0-1][0-9]|92[0-5]|64[4-9])|65)/,
+        valid_length: [16]
       }
     ];
-    check_card = function(number) {
-      var card, _i, _len;
-      for (_i = 0, _len = cards.length; _i < _len; _i++) {
-        card = cards[_i];
-        if (number.match(card.pattern)) return callback(card.name);
+    get_card_type = function(number) {
+      var card_type, _i, _len;
+      for (_i = 0, _len = card_types.length; _i < _len; _i++) {
+        card_type = card_types[_i];
+        if (number.match(card_type.pattern)) return card_type;
       }
-      return callback('');
+      return null;
     };
-    validate_luhn = function(number) {
-      var check_digit, digit, n, sum, _len, _ref, _results;
-      check_digit = number[number.length - 1];
+    is_valid_luhn = function(number) {
+      var digit, n, sum, _len, _ref;
       sum = 0;
-      _ref = number.split('').reverse().slice(1).join('');
-      _results = [];
+      _ref = number.split('').reverse().join('');
       for (n = 0, _len = _ref.length; n < _len; n++) {
         digit = _ref[n];
-        n = parseInt(n, 10);
-        digit = parseInt(digit, 10);
+        digit = +digit;
         if (n % 2) {
-          _results.push(sum += digit);
-        } else {
           digit *= 2;
           if (digit < 10) {
-            _results.push(sum += digit);
+            sum += digit;
           } else {
-            _results.push(sum += parseInt(digit.toString()[0], 10) + parseInt(digit.toString()[1], 10));
+            sum += digit - 9;
           }
+        } else {
+          sum += digit;
         }
       }
-      return _results;
+      return sum % 10 === 0;
+    };
+    is_valid_length = function(number, card_type) {
+      var _ref;
+      return _ref = number.length, __indexOf.call(card_type.valid_length, _ref) >= 0;
+    };
+    validate = function(number) {
+      var card_type, length, luhn;
+      card_type = get_card_type(number);
+      if (card_type != null) {
+        luhn = is_valid_luhn(number);
+        length = is_valid_length(number, card_type);
+      }
+      return callback({
+        card_type: card_type,
+        luhn: luhn,
+        length: length
+      });
     };
     this.bind('input', function() {
       var number;
       number = $(this).val().replace(/[ -]/, '');
-      validate_luhn(number);
-      return check_card(number);
+      return validate(number);
     });
     number = $(this).val().replace(/[ -]/, '');
-    check_card(number);
+    validate(number);
     return this;
   };
 
