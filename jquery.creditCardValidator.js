83ba577473d8319f09bd3b04ea341f28843cc208
Pawel Decowski <pawel.decowski@gmail.com>
2012-02-22 12:21:03 +0000

    Added licence text

--- jquery.creditCardValidator.js
+++ jquery.creditCardValidator.js
@@ -1,3 +1,20 @@
+
+/*
+jQuery Credit Card Validation
+
+Copyright 2012 Pawel Decowski
+
+This work is licensed under the Creative Commons Attribution-ShareAlike 3.0
+Unported License. To view a copy of this license, visit:
+
+http://creativecommons.org/licenses/by-sa/3.0/
+
+or send a letter to:
+
+Creative Commons, 444 Castro Street, Suite 900,
+Mountain View, California, 94041, USA.
+*/
+
 (function() {
   var $,
     __indexOf = Array.prototype.indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
